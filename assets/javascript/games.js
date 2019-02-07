a8bd94a760d8b653a44df5d708b0e1ee0f75ff6a
Szewah <swgrleong@gmail.com>
2019-02-07 13:19:57 -0500

    Second commit

---
+++ assets/javascript/games.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+    var randomNumber = Math.floor(Math.random()*100 + 1);
+    var num1 = Math.floor(Math.random()*10 + 1);
+    var num2 = Math.floor(Math.random()*10 + 1);
+    var num3 = Math.floor(Math.random()*10 + 1);
+    var num4 = Math.floor(Math.random()*10 + 1);
+    var score =0;
+    var wins = 0;
+    var losses = 0;
+
+    function startGame() {
+        score = 0;
+        randomNumber =Math.floor(Math.random()*100 + 1)
+        console.log(randomNumber);
+        num1 = Math.floor(Math.random()*10 + 1);
+        num2 = Math.floor(Math.random()*10 + 1);
+        num3 = Math.floor(Math.random()*10 + 1);
+        num4 = Math.floor(Math.random()*10 + 1);
+        $('#number').text(randomNumber);
+        $('#wins').text('Wins: ' + wins);
+        $('#losses').text('Losses: ' + losses);
+    };
+
+    startGame();
+
+    $('#colorImage').on('click', function() {
+        score += num1;
+        winlose();
+        $('#scoreBox').text(score);
+    });
+
+    $('#blackWhiteImage').on('click', function() {
+        score += num2;
+        winlose();
+        $('#scoreBox').text(score);
+    });
+
+    $('#goldBlackImage').on('click', function() {
+        score += num3;
+        winlose();
+        $('#scoreBox').text(score);
+    });
+
+    $('#pink').on('click', function() {
+        score += num4;
+        winlose();
+        $('#scoreBox').text(score);
+    });
+
+    function winlose() {
+        if (score === randomNumber) {
+            wins++;
+            alert('You won');
+            $('#wins').text('Wins: ' + wins);
+            startGame(); 
+        } else if (score > randomNumber) {
+            losses++;
+            alert('You lost');
+            $('#losses').text('Losses: ' + losses);
+            startGame(); 
+        }       
+    };
+});
